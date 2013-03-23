175139bf0fea5343c75616854bd42acb0aa60d3e
Nathan Peck <nathan@storydesk.com>
2013-03-23 12:45:42 -0400

    Fixing typo in my comment, and adding the MongoDB lib as a dependency.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var program = require('commander'); //Commandline option support.
 var packageJson = require('./package.json'); //Grab the data from the package.json file.
 
 program
-  .version(packageJson.version) //Dynamic verison numer based on version in package.json
+  .version(packageJson.version) //Dynamic verison number based on version in package.json
   .option('-w, --hello', 'Print hello world')
 	.parse(process.argv);
 
