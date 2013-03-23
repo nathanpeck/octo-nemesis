e72926021aa7fc7fec46de93d1f8aa09bb16c6d9
Jason <jason@absurdnerd.com>
2013-03-23 18:15:15 +0000

    test of database connection.

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ var packageJson = require('./package.json'); //Grab the data from the package.js
 
 program
   .version(packageJson.version) //Dynamic verison number based on version in package.json
+  .option('-d, --dbconn', 'Test connection')
   .option('-w, --hello', 'Print hello world')
 	.parse(process.argv);
 
 if(program.hello)
 	console.log('Hello World!');
+if(program.dbconn)
+{
+	var MongoClient = require('mongodb').MongoClient , Server = require('mongodb').Server;
+	var mongoClient = new MongoClient(new Server('localhost', 27017));
+	
+	mongoClient.open(function(err, mongoClient) {
+	  var db1 = mongoClient.db("octo-nemesis");
+	  console.log("test");
+	
+	  mongoClient.close();
+	});
+}
 else
 	program.help();
