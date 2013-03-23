ab8210b198bea979d6c0c80123ac482581718d16
Nathan Peck <nathan@storydesk.com>
2013-03-23 16:27:27 +0000

    Adding a basic commandline app framework to work with.

---
+++ app.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var program = require('commander'); //Commandline option support.
+var packageJson = require('./package.json'); //Grab the data from the package.json file.
+
+program
+  .version(packageJson.version) //Dynamic verison numer based on version in package.json
+  .option('-w, --hello', 'Print hello world')
+	.parse(process.argv);
+
+if(program.hello)
+	console.log('Hello World!');
+else
+	program.help();
