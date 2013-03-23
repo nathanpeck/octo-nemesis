192d7e6a5ac86c4aee26ad838d321aebba2dbea2
Nathan Peck <nathan@storydesk.com>
2013-03-23 14:36:25 -0400

    Adding a the start of a basic server component for doing OAuth with Instagram. For now it is mostly just a boilerplate placeholder.

--- app.js
+++ app.js
@@ -2,13 +2,44 @@
 
 var program = require('commander'); //Commandline option support.
 var packageJson = require('./package.json'); //Grab the data from the package.json file.
+var http = require('http');
+var fs = require('fs');
 
 program
   .version(packageJson.version) //Dynamic verison number based on version in package.json
-  .option('-w, --hello', 'Print hello world')
+  .option('-i, --instagram <action>', 'Instagram integration [server] to start O-Auth component')
 	.parse(process.argv);
 
-if(program.hello)
-	console.log('Hello World!');
+if(program.instagram)
+{
+	if(program.instagram.length === 0)
+	{
+		//Output usage details.
+		console.log('\nYou must specify one of the following actions to integrate with Instagram:\n');
+		console.log('  server  -   Start a server for doing an OAuth with Instagram to get API credentials');
+		console.log('  scan    -   Scan Instagram for new photos not in your stream yet.');
+		console.log('              Requires OAuth tokens for API access.\n');
+	}
+	else
+	{
+		if(program.instagram[0] == 'server')
+		{
+			//Start up an Instagram OAuth Server component.
+			var express = require('express');
+			var app = express();
+
+			app.use(express.static('static'));
+
+			app.get('/', function(req, res){
+				res.send('hello world');
+			});
+
+			app.listen(3000);
+		}
+	}
+}
 else
+{
 	program.help();
+}
+
